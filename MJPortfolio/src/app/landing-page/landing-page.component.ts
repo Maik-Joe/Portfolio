8238ed89b4b52a85f8e63fd05e13d3017d9a1b88
Maik Joe <maik-joe@gmx.de>
2025-02-12 18:30:48 +0100

    shared c

--- MJPortfolio/src/app/landing-page/landing-page.component.ts
+++ MJPortfolio/src/app/landing-page/landing-page.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "../header/header.component";
 import { HeroComponent } from "../hero/hero.component";
+import { HeaderComponent } from '../shared/header/header.component';
+import { FooterComponent } from '../shared/footer/footer.component';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [HeaderComponent, HeroComponent],
+  imports: [HeroComponent,
+            HeaderComponent,
+            FooterComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
