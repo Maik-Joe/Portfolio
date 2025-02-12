a5c9801107907f8c5e4b213773de854dbc892b0a
Maik Joe <maik-joe@gmx.de>
2025-02-12 21:04:44 +0100

    aboutMe

---
+++ MJPortfolio/src/app/about-me/about-me.component.ts
@@ -0,0 +1,22 @@
+import { Component, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'app-about-me',
+  templateUrl: './about-me.component.html',
+  styleUrls: ['./about-me.component.scss']
+})
+export class AboutMeComponent {
+
+  @HostListener('window:scroll', [])
+  onScroll(): void {
+    const lines = document.querySelectorAll('.line') as NodeListOf<HTMLElement>;
+    lines.forEach((line) => {
+      const rect = line.getBoundingClientRect();
+      if (rect.top < window.innerHeight && rect.bottom >= 0) {
+        line.style.width = '60%';
+      } else {
+        line.style.width = '0';
+      }
+    });
+  }
+}
--- MJPortfolio/src/app/landing-page/landing-page.component.ts
+++ MJPortfolio/src/app/landing-page/landing-page.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { HeroComponent } from "../hero/hero.component";
 import { HeaderComponent } from '../shared/header/header.component';
 import { FooterComponent } from '../shared/footer/footer.component';
+import { AboutMeComponent } from "../about-me/about-me.component";
 
 @Component({
   selector: 'app-landing-page',
   imports: [HeroComponent,
-            HeaderComponent,
-            FooterComponent],
+    HeaderComponent,
+    FooterComponent, AboutMeComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
