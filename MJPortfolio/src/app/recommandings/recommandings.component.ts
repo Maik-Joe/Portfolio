c8e1cdc3e7fbdfb38b8cc40b2d4ab79739d68f66
Maik Joe <maik-joe@gmx.de>
2025-02-20 13:30:34 +0100

    recommandings3.0

--- MJPortfolio/src/app/recommandings/recommandings.component.ts
+++ MJPortfolio/src/app/recommandings/recommandings.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-recommandings',
-  imports: [],
   templateUrl: './recommandings.component.html',
   styleUrl: './recommandings.component.scss'
 })
 export class RecommandingsComponent {
+  activeBox: HTMLElement | null = null;
 
+  constructor(private renderer: Renderer2, private el: ElementRef) {}
+
+  toggleActive(event: Event) {
+    const target = event.currentTarget as HTMLElement;
+
+    if (this.activeBox === target) {
+      this.renderer.removeClass(target, 'active');
+      this.activeBox = null;
+    } else {
+      if (this.activeBox) {
+        this.renderer.removeClass(this.activeBox, 'active');
+      }
+      this.renderer.addClass(target, 'active');
+      this.activeBox = target;
+    }
+  }
 }
